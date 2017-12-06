f2758c599ea7e9baa6e438bd6a7e7fcc9223713b
julia- <julia.ryanx@gmail.com>
2017-12-06 12:17:55 +1100

    add products route

--- api/server.js
+++ api/server.js
@@ -3,7 +3,13 @@ const bodyParser = require('body-parser')
 
 const server = express()
 
+// Middleware
 server.use(bodyParser.json())
+
+// Routes
+server.use([
+  require('./routes/products')
+])
 
 server.listen(7000, (error) => {
   if (error) {
