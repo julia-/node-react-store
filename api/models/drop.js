ba6af7d16ac235b7684e7c5871c78be73d8676e8
julia- <julia.ryanx@gmail.com>
2017-12-06 13:21:32 +1100

    add delete function for all users

--- api/models/drop.js
+++ api/models/drop.js
@@ -1,7 +1,13 @@
 const Product = require('./Product')
+const Product = require('./User')
 
 Product.deleteMany()
   .then(() => {
     console.log('Deleted products')
+  })
+
+User.deleteMany()
+  .then(() => {
+    console.log('Deleted users')
     process.exit()
   })
