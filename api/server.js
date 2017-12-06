2eef6f249341c93d33e27d8eb995487804877439
julia- <julia.ryanx@gmail.com>
2017-12-06 11:42:28 +1100

    add express server

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const server = express()
+
+server.use(bodyParser.json())
+
+server.listen(7000, (error) => {
+  if (error) {
+    console.error('Error starting' error)
+  } else {
+    console.log('Server started at http://localhost:7000');
+  }
+})
