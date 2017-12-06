9652217c886e6127acff7b31c51172f041d7240c
julia- <julia.ryanx@gmail.com>
2017-12-06 11:44:51 +1100

    add missing comma

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ server.use(bodyParser.json())
 
 server.listen(7000, (error) => {
   if (error) {
-    console.error('Error starting' error)
+    console.error('Error starting', error)
   } else {
     console.log('Server started at http://localhost:7000');
   }
