0c9c69cba88ce11d1a0d27c6877a9f58e9b391a2
julia- <julia.ryanx@gmail.com>
2017-12-06 12:05:18 +1100

    add Product model with seeds

---
+++ api/models/seeds.js
@@ -0,0 +1,28 @@
+const Product = require('./Product')
+
+Product.create([
+  {
+    brandName: 'The Horse',
+    name: 'The Horse Original Watch'
+  },
+  {
+    brandName: 'Swell',
+    name: 'Swell Bottle 500ml'
+  },
+  {
+    brandName: 'Bellroy',
+    name: 'Bellroy Hide & Seek Wallet'
+  },
+  {
+    brandName: 'Bellroy',
+    name: 'Bellroy Pencil Case'
+  },
+  {
+    brandName: 'Herschel Supply Co.',
+    name: 'Little America 15 Laptop Backpack'
+  },
+  {
+    brandName: 'Orbit',
+    name: 'Orbit Key Finder'
+  }
+])
