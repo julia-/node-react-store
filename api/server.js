23a9469860fb0635f6248555543a8f7c480e50f8
julia- <julia.ryanx@gmail.com>
2017-12-07 11:49:46 +1100

    connect signin to api

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const authMiddleware = require('./middleware/auth')
 
 const server = express()
 
 // Middleware
 server.use(bodyParser.json()) // Allows JSON uploads (POST/PUT/PATCH)
+server.use(cors()) // ALlow other origins to access i.e. react front-end
 server.use(authMiddleware.initialize) // Start passport
 
 // Routes
