4001e029b0aa180660f2da7c36feb8bf99e9e633
julia- <julia.ryanx@gmail.com>
2017-12-08 12:54:11 +1100

    add new product and remove product from wishlist routes

--- api/models/Wishlist.js
+++ api/models/Wishlist.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const wishlistSchema = new Schema({
   user: { type: Schema.ObjectId, ref: 'User', unique: true }, // User can only have one wishlist
-  product: [{ type: Schema.ObjectId, ref: 'Product' }]
+  products: [{ type: Schema.ObjectId, ref: 'Product' }]
 })
 
 const Wishlist = mongoose.model('Wishlist', wishlistSchema)
--- api/routes/wishlist.js
+++ api/routes/wishlist.js
@@ -6,6 +6,7 @@ const router = new express.Router()
 
 router.get('/wishlist', requireJWT, (req, res) => {
   Wishlist.findOne({ user: req.user })
+  .populate('products')
     .then(wishlist => {
       if (wishlist) {
         res.json({ products: wishlist.products })
@@ -17,6 +18,48 @@ router.get('/wishlist', requireJWT, (req, res) => {
     .catch(error => {
       res.status(500).json({ error })
     })
+})
+
+router.post('/wishlist/products/:productID', requireJWT, (req, res) => {
+  const { productID } = req.params
+  Wishlist.findOneAndUpdate(
+    // Find wishlist for signed in user
+    { user: req.user },
+    // Make these changes
+    { $addToSet: { products: productID } },
+    // Options when updating
+    // upsert: Update is exists, inserts (create) if not
+    // new: give the update wishlist
+    { upsert: true, new: true, runValidators: true }
+  )
+    .populate('products')
+    .then(wishlist => {
+      res.json({ products: wishlist.products })
+    })
+    .catch(error => {
+      res.status(400).json({ error })
+    })
+})
+
+router.delete('/wishlist/products/:productID', requireJWT, (req, res) => {
+  const { productID } = req.params
+    Wishlist.findOneAndUpdate(
+      // Find wishlist for signed in user
+      { user: req.user },
+      // Make these changes
+      { $pull: { products: productID } },
+      // Options when updating
+      // upsert: Update is exists, inserts (create) if not
+      // new: give the update wishlist
+      { upsert: true, new: true, runValidators: true }
+    )
+      .populate('products')
+      .then(wishlist => {
+        res.json({ products: wishlist.products })
+      })
+      .catch(error => {
+        res.status(400).json({ error })
+      })
 })
 
 module.exports = router
