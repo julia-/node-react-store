69ff0207ae52528a4a9d58a965ca55f66b76e0da
julia- <julia.ryanx@gmail.com>
2017-12-08 14:24:33 +1100

    add product routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,7 +10,47 @@ router.get('/products', authMiddleware.requireJWT, (req, res) => {
       res.json(products)
     })
     .catch(error => {
-      res.json({ error })
+      res.status(400).json({ error })
+    })
+})
+
+router.get('/products/:id', authMiddleware.requireJWT, (req, res) => {
+  const id = req.params.id
+  Product.findById(id)
+    .then(product => {
+      res.json(product)
+    })
+    .catch(error => {
+      res.status(400).json({ error })
+    })
+})
+
+router.post('/products', authMiddleware.requireJWT, (req, res) => {
+  const attributes = req.body
+  Product.create(attributes)
+  .then(product => {
+    res.status(201).json(product)
+  })
+  .catch(error => {
+    res.status(400).json({ error })
+  })
+})
+
+router.put('/products/:id', authMiddleware.requireJWT, (req, res) => {
+  const { id } = req.params
+  Product.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+    .then(product => {
+      if (product) {
+        res.json(product)
+      }
+      else {
+        res.status(404).json({
+          error: new Error(`Product with id '${id}' not found`)
+        })
+      }
+    })
+    .catch(error => {
+      res.status(400).json({ error })
     })
 })
 
